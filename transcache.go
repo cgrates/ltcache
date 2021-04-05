0bc8d86e35d7fcc69ec259593840387787b20a57
ionutboangiu <ionut.boangiu@itsyscom.com>
2021-04-05 15:54:31 +0300

    Update GenUUID func in transcache.go

--- transcache.go
+++ transcache.go
@@ -31,10 +31,7 @@ var (
 
 func GenUUID() string {
 	b := make([]byte, 16)
-	_, err := io.ReadFull(rand.Reader, b)
-	if err != nil {
-		return ""
-	}
+	io.ReadFull(rand.Reader, b)
 	b[6] = (b[6] & 0x0F) | 0x40
 	b[8] = (b[8] &^ 0x40) | 0x80
 	return fmt.Sprintf("%x-%x-%x-%x-%x", b[:4], b[4:6], b[6:8], b[8:10],
